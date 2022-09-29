01500fbc21845291e5177fa21cffe071234ac53f
awres <awres99@gmail.com>
2022-09-29 13:28:41 +0200

    Update Home.tsx

--- unityApp/screens/Home.tsx
+++ unityApp/screens/Home.tsx
@@ -1,30 +1,73 @@
 import React, { useState } from 'react';
-import { Button, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 
 const Home = ({ navigation, route }: { navigation: undefined, route: any }) => {
-  const [shape, setShape] = useState("");
-  const [color, setColor] = useState("");
-  var lastShape = "";
-  var lastColor = "";
+  const [playerName, setPlayerName] = useState("");
+  const [roomName, setRoomName] = useState("");
+  var lastPlayerName = "";
+  var lastRoomName = "";
   var isTextVisible = false;
 
   if (route?.params) {
     isTextVisible = true;
-    lastShape = route.params.shape;
-    lastColor = route.params.color;
+    lastPlayerName = route.params.shape;
+    lastRoomName = route.params.color;
   }
 
-  const goToUnity = () => {
-    navigation.navigate('Unity', {
-      shape: shape,
-      color: color
-    });
+  const goToUnity = (param: boolean) => {
+    setTimeout(() =>
+      navigation.navigate('Unity', {
+        playerName: playerName,
+        host: param,
+        roomName: roomName,
+        playerColor: "Red",
+      }), 500);
+
   }
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Picker style={{ width: 400 }}
+      <Text>Player Name:</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setPlayerName}
+        value={playerName}
+      />
+      <Text>Room Name:</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setRoomName}
+        value={roomName}
+      />
+      <Button disabled={playerName == "" || roomName == ""} title="Join room" onPress={() => goToUnity(false)} />
+      <Button disabled={playerName == "" || roomName == ""} title="Host room" onPress={() => goToUnity(true)} />
+      <View style={{ width: 400, padding: 20 }}>
+        {isTextVisible && <Text style={styles.text}>Last player name: {playerName}</Text>}
+        {isTextVisible && <Text style={styles.text}>Last room name: {roomName}</Text>}
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    width: 350,
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  text: {
+    fontSize: 17,
+    color: 'black',
+  }
+});
+
+export default Home;
+
+/*
+  <Picker style={{ width: 400 }}
         selectedValue={shape}
         onValueChange={(itemValue) =>
           setShape(itemValue)
@@ -49,8 +92,4 @@ const Home = ({ navigation, route }: { navigation: undefined, route: any }) => {
         {isTextVisible && <Text style={{ fontSize: 17 }}>Last shape: {lastShape}</Text>}
         {isTextVisible && <Text style={{ fontSize: 17 }}>Last color: {lastColor}</Text>}
       </View>
-    </View>
-  );
-};
-
-export default Home;
+*/
